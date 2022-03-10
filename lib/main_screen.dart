91ffa0600147f7354f083ff8bbe9be5f49d2c764
Abhishek Chougule <chouguleabhis@gmail.com>
2022-03-10 10:39:27 +0530

    ios lib

---
+++ lib/main_screen.dart
@@ -0,0 +1,195 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:fitness_pro/main.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'pushed_pageA.dart';
+import 'pushed_pageS.dart';
+import 'pushed_pageY.dart';
+
+class MainScreen extends StatelessWidget {
+  final List<CameraDescription> cameras;
+  MainScreen(this.cameras);
+
+  static const String id = 'main_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 50),
+          Container(
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text(
+              'FitnessPro',
+              style: TextStyle(
+                color: Color(0xFFFE7C7C),
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text(
+              'Master Your Body Alignment',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 50),
+          Image.asset('images/align.PNG' ,height: 100, width: 500,),
+          SizedBox(height: 50),
+          
+          SizedBox(height: 20),
+          Container(
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text(
+              'Strength Alignment',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 24.0,
+              ),
+            ),
+          ),
+          Container(
+            child: SizedBox(
+              height: 150,
+              child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                scrollDirection: Axis.horizontal,
+                children: [
+                 
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 140,
+                        height: 140,
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0)),
+                          color: Colors.white,
+                          child: Container(
+                              padding: EdgeInsets.all(10.0),
+                              child: Image.asset('images/arm_press.PNG')),
+                          onPressed: () =>
+                              onSelectA(context: context, modelName: 'posenet'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 140,
+                        height: 140,
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0)),
+                          color: Colors.white,
+                          child: Container(
+                              padding: EdgeInsets.all(10.0),
+                              child: Image.asset('images/squat.PNG')),
+                          onPressed: () =>
+                              onSelectS(context: context, modelName: 'posenet'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Container(
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text(
+              'Yoga Alignment',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 24.0,
+              ),
+            ),
+          ),
+          Container(
+            child: SizedBox(
+              height: 150,
+              child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 140,
+                        height: 140,
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0)),
+                          color: Colors.white,
+                          child: Container(
+                              padding: EdgeInsets.all(10.0),
+                              child: Image.asset('images/yoga4.PNG')),
+                          onPressed: () =>
+                              onSelectY(context: context, modelName: 'posenet'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void onSelectA({BuildContext context, String modelName}) async {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => PushedPageA(
+        cameras: cameras,
+        title: modelName,
+      ),
+    ),
+  );
+}
+
+void onSelectS({BuildContext context, String modelName}) async {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => PushedPageS(
+        cameras: cameras,
+        title: modelName,
+      ),
+    ),
+  );
+}
+
+void onSelectY({BuildContext context, String modelName}) async {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => PushedPageY(
+        cameras: cameras,
+        title: modelName,
+      ),
+    ),
+  );
+}
